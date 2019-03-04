49b3d32b4207262d7ff69003596ba7e46ada1040
smet_k <ski97@mail.ru>
2019-03-04 12:13:28 +0300

    [hello_world] changed portIn to port

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -6,15 +6,12 @@ import (
 	"net/http"
 )
 
-func Run(portIn string) error {
-	if portIn == "" {
-		portIn = "5051"
-	}
-	portFull := ":" + portIn
+func Run(port string) error {
+	address := ":" + port
 
 	router := mux.NewRouter()
 
 	router.HandleFunc("/hello", controllers.HelloWorld).Methods("GET")
 
-	return http.ListenAndServe(portFull, router)
+	return http.ListenAndServe(address, router)
 }
