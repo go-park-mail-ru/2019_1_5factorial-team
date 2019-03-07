784a90a61cb8d99c984cde1024e333cc69007b8f
smet_k <ski97@mail.ru>
2019-03-07 18:34:24 +0300

    [auth] middleware not returning error on invalid cookie, just reseting it

--- internal/pkg/middleware/auth.go
+++ internal/pkg/middleware/auth.go
@@ -15,35 +15,47 @@ func AuthMiddleware(next http.Handler) http.Handler {
 		cookie, err := req.Cookie("token")
 		if err != nil {
 
-			ctx = context.WithValue(ctx, "realId", -1)
+			ctx = context.WithValue(ctx, "userID", -1)
 			ctx = context.WithValue(ctx, "authorized", false)
+			ctx = context.WithValue(ctx, "token", "")
 		} else {
 
 			uId, err := session.GetId(cookie.Value)
+			// TODO(smet1):
+			//if err != nil {
+			//	// invalid token
+			//	// KILL HIM
+			//	// TODO(smet1): ничего умнее не придумал
+			//
+			//	cookie.Expires = time.Unix(0, 0)
+			//	http.SetCookie(res, cookie)
+			//	http.Error(res, "cookie invalid, relogin please", http.StatusTeapot)
+			//	return
+			//}
+
 			if err != nil {
-				// invalid token
-				// KILL HIM
-				// TODO(smet1): ничего умнее не придумал
+				ctx = context.WithValue(ctx, "userID", -1)
+				ctx = context.WithValue(ctx, "authorized", false)
+				ctx = context.WithValue(ctx, "token", "")
 
 				cookie.Expires = time.Unix(0, 0)
 				http.SetCookie(res, cookie)
-				http.Error(res, "cookie invalid, relogin please", http.StatusTeapot)
-				return
-			}
 
-			ctx = context.WithValue(ctx, "realId", uId)
-			ctx = context.WithValue(ctx, "authorized", true)
-			ctx = context.WithValue(ctx, "token", cookie.Value)
+			} else {
+				ctx = context.WithValue(ctx, "userID", uId)
+				ctx = context.WithValue(ctx, "authorized", true)
+				ctx = context.WithValue(ctx, "token", cookie.Value)
 
-			// сетим новое время куки
-			// юзер может придти, когда его токен испортиться в момент обращения к серверу,
-			// поэтому добавлю его текущий токен
-			updatedToken, err := session.UpdateToken(cookie.Value, uId)
-			if err != nil {
-				http.Error(res, "relogin, please", http.StatusInternalServerError)
+				// сетим новое время куки
+				// юзер может придти, когда его токен испортиться в момент обращения к серверу,
+				// поэтому добавлю его текущий токен
+				updatedToken, err := session.UpdateToken(cookie.Value, uId)
+				if err != nil {
+					http.Error(res, "relogin, please", http.StatusInternalServerError)
+				}
+				cookie.Expires = updatedToken.CookieExpiredTime
+				http.SetCookie(res, cookie)
 			}
-			cookie.Expires = updatedToken.CookieExpiredTime
-			http.SetCookie(res, cookie)
 		}
 
 		next.ServeHTTP(res, req.WithContext(ctx))
