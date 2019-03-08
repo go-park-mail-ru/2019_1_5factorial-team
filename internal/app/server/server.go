c285c19a6970ece391130b5208f2c6da6df44b33
smet_k <ski97@mail.ru>
2019-03-08 15:14:21 +0300

    [auth] using subrouter for second middleware

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -18,8 +18,14 @@ func Run(port string) error {
 	router.HandleFunc("/hello", controllers.HW).Methods("GET")
 	router.HandleFunc("/api/user", controllers.SignUp).Methods("POST")
 	router.HandleFunc("/api/session", controllers.SignIn).Methods("POST")
-	router.HandleFunc("/api/session", controllers.SignOut).Methods("DELETE")
-	router.HandleFunc("/api/user", controllers.GetUserFromSession).Methods("GET")
+
+	routerLoginRequired := router.PathPrefix("").Subrouter()
+
+	routerLoginRequired.Use(middleware.CheckLoginMiddleware)
+
+	routerLoginRequired.HandleFunc("/api/user", controllers.GetUserFromSession).Methods("GET")
+	routerLoginRequired.HandleFunc("/api/session", controllers.IsSessionValid).Methods("GET")
+	routerLoginRequired.HandleFunc("/api/session", controllers.SignOut).Methods("DELETE")
 
 	err := http.ListenAndServe(address, router)
 	if err != nil {
