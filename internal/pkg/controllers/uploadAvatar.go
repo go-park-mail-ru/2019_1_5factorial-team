2c77416d2dbe966a69e8fe15c3f7595f14fb2d19
smet_k <ski97@mail.ru>
2019-03-13 01:41:42 +0300

    [dev] added some logs on upload avatar

--- internal/pkg/controllers/uploadAvatar.go
+++ internal/pkg/controllers/uploadAvatar.go
@@ -26,38 +26,39 @@ type AvatarLinkResponse struct {
 // @Failure 400 {object} controllers.errorResponse
 // @Router /api/upload_avatar [post]
 func UploadAvatar(res http.ResponseWriter, req *http.Request) {
+	log.Println(req.URL, "UploadAvatar")
 
 	// проверка на максимально допустимый размер
 	req.Body = http.MaxBytesReader(res, req.Body, fileproc.MaxUploadSize)
 	if err := req.ParseMultipartForm(fileproc.MaxUploadSize); err != nil {
 		ErrResponse(res, http.StatusBadRequest, "file too big")
-		log.Println(errors.Wrap(err, "file too big"))
 
+		log.Println(errors.Wrap(err, "file too big"))
 		return
 	}
 
 	file, headers, err := req.FormFile("upload")
 	if err != nil {
-		ErrResponse(res, http.StatusBadRequest, "invalied file in request")
-		log.Println(errors.Wrap(err, "invalied file in request"))
+		ErrResponse(res, http.StatusBadRequest, "invalid file in request")
 
+		log.Println(errors.Wrap(err, "invalid file in request"))
 		return
 	}
 
 	defer file.Close()
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		ErrResponse(res, http.StatusBadRequest, "invalied file cant readall")
-		log.Println(errors.Wrap(err, "invalied file cant readall"))
+		ErrResponse(res, http.StatusBadRequest, "invalid file cant ReadAll")
 
+		log.Println(errors.Wrap(err, "invalid file cant ReadAll"))
 		return
 	}
 
 	filetype := http.DetectContentType(fileBytes)
 	if !fileproc.CheckFileType(filetype) {
-		ErrResponse(res, http.StatusBadRequest, "invalied file type")
-		log.Println(errors.Wrap(err, "invalied file type"))
+		ErrResponse(res, http.StatusBadRequest, "invalid file type")
 
+		log.Println(errors.Wrap(err, "invalid file type"))
 		return
 	}
 
@@ -66,8 +67,8 @@ func UploadAvatar(res http.ResponseWriter, req *http.Request) {
 	fileExtension := filepath.Ext(headers.Filename)
 	if err != nil {
 		ErrResponse(res, http.StatusInternalServerError, "cant read file type")
-		log.Println(errors.Wrap(err, "cant read file type"))
 
+		log.Println(errors.Wrap(err, "cant read file type"))
 		return
 	}
 
@@ -75,12 +76,16 @@ func UploadAvatar(res http.ResponseWriter, req *http.Request) {
 	resultFile, err := fileproc.CreateResultFile(fileName, fileExtension, filetype, fileBytes)
 	if err != nil {
 		ErrResponse(res, http.StatusInternalServerError, "cant write file")
-		log.Println(errors.Wrap(err, "cant write file"))
 
+		log.Println(errors.Wrap(err, "cant write file"))
 		return
 	}
+
+	log.Println("\t", req.URL, "OkResponse")
+	log.Println("\t\tavatar_link:", resultFile)
+	log.Println("\t\tfileExtension:", fileExtension, ", filetype:", filetype)
+
 	OkResponse(res, AvatarLinkResponse{
 		AvatarLink: resultFile,
 	})
-
 }
