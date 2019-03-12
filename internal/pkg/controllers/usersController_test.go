0f3cf4dcd2544e9109d721cd893e4932b4ab47e1
smet_k <ski97@mail.ru>
2019-03-13 02:14:16 +0300

    [tests] start signUp tests

--- internal/pkg/controllers/usersController_test.go
+++ internal/pkg/controllers/usersController_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -14,9 +15,10 @@ type TestCases struct {
 	urlValues      string
 	expectedRes    string
 	expectedStatus int
+	authCtx bool
 }
 
-var test = []TestCases{
+var testsGetUser = []TestCases{
 	{
 		method:         "GET",
 		url:            "/api/user/",
@@ -27,10 +29,10 @@ var test = []TestCases{
 	},
 }
 
-func TestSignUp(t *testing.T) {
+func TestGetUser(t *testing.T) {
 	var req *http.Request
 	var err error
-	for _, val := range test {
+	for _, val := range testsGetUser {
 
 		req, err = http.NewRequest(val.method, val.url, val.body)
 		if err != nil {
@@ -39,6 +41,47 @@ func TestSignUp(t *testing.T) {
 
 		rr := httptest.NewRecorder()
 		handler := http.HandlerFunc(GetUser)
+
+		handler.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != val.expectedStatus {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, val.expectedStatus)
+		}
+
+		// Check the response body is what we expect.
+		if rr.Body.String() != val.expectedRes {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), val.expectedRes)
+		}
+	}
+}
+
+var testsSignUp = []TestCases{
+	{
+		method:         "POST",
+		url:            "/api/user",
+		body:           strings.NewReader(`{"email": "kek@email.kek",}`),
+		urlValues:      "",
+		expectedRes:    `{{"error":"json parsing error: invalid character '}' looking for beginning of object key string"}`,
+		expectedStatus: http.StatusInternalServerError,
+		authCtx:false,
+	},
+}
+
+func TestSignUp(t *testing.T) {
+	var req *http.Request
+	var err error
+	for _, val := range testsGetUser {
+
+		req, err = http.NewRequest(val.method, val.url, val.body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(SignUp)
 
 		handler.ServeHTTP(rr, req)
 
