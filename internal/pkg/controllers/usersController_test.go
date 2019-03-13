5f2cef0d91f675e433c623315d7663891177b6e7
smet_k <ski97@mail.ru>
2019-03-13 04:40:46 +0300

    [tests] testin via my super-duper func

--- internal/pkg/controllers/usersController_test.go
+++ internal/pkg/controllers/usersController_test.go
@@ -2,9 +2,13 @@ package controllers
 
 import (
 	"context"
+	"github.com/go-park-mail-ru/2019_1_5factorial-team/internal/pkg/user"
+	"github.com/pkg/errors"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -30,6 +34,76 @@ var testsGetUser = []TestCases{
 	},
 }
 
+func testHandler(funcToTest func(http.ResponseWriter, *http.Request), tests []TestCases) error {
+	var req *http.Request
+	var err error
+	for _, val := range tests {
+
+		req, err = http.NewRequest(val.method, val.url, val.body)
+		if err != nil {
+			return err
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(funcToTest)
+
+		ctx := req.Context()
+		ctx = context.WithValue(ctx, "authorized", val.authCtx)
+		req = req.WithContext(ctx)
+
+		handler.ServeHTTP(rr, req)
+
+		// try to check cookie, LOOKS LIKE HACK
+		kek := rr.Header()["Set-Cookie"]
+		if kek != nil {
+			log.Println(strings.Split(kek[0], ";"))
+		}
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != val.expectedStatus {
+			return errors.Errorf("handler returned wrong status code: \ngot  %v \nwant %v",
+				status, val.expectedStatus)
+		}
+
+		// Check the response body is what we expect.
+		if rr.Body.String() != val.expectedRes {
+			return errors.Errorf("handler returned unexpected body: \ngot  %v \nwant %v",
+				rr.Body.String(), val.expectedRes)
+		}
+	}
+	return nil
+}
+
+var testsUsersCountInfo = []TestCases{
+	{
+		method:         "GET",
+		url:            "/api/user/count",
+		body:           nil,
+		urlValues:      "",
+		expectedRes:    `{"count":` + strconv.Itoa(user.GetUsersCount()) + `}`,
+		expectedStatus: http.StatusOK,
+		authCtx:        false,
+	},
+	{
+		method:         "GET",
+		url:            "/api/user/count",
+		body:           nil,
+		urlValues:      "",
+		// сейчас 21, но хз сколько будет
+		expectedRes:    `{"count":` + strconv.Itoa(user.GetUsersCount()) + `}`,
+		expectedStatus: http.StatusOK,
+		authCtx:        true,
+	},
+}
+
+func TestUsersCountInfo(t *testing.T) {
+	err := testHandler(UsersCountInfo, testsUsersCountInfo)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+// undone
 func TestGetUser(t *testing.T) {
 	var req *http.Request
 	var err error
@@ -99,34 +173,66 @@ var testsSignUp = []TestCases{
 }
 
 func TestSignUp(t *testing.T) {
-	var req *http.Request
-	var err error
-	for _, val := range testsSignUp {
+	err := testHandler(SignUp, testsSignUp)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
 
-		req, err = http.NewRequest(val.method, val.url, val.body)
-		if err != nil {
-			t.Fatal(err)
-		}
+// get leaderboard
 
-		rr := httptest.NewRecorder()
-		handler := http.HandlerFunc(SignUp)
 
-		ctx := req.Context()
-		ctx = context.WithValue(ctx, "authorized", val.authCtx)
-		req = req.WithContext(ctx)
+var testsSignIn = []TestCases{
+	{
+		method:         "POST",
+		url:            "/api/user",
+		body:           strings.NewReader(`{"loginOrEmail": "kek@email.kek"}`),
+		urlValues:      "",
+		expectedRes:    `{"error":"Wrong password or login"}`,
+		expectedStatus: http.StatusBadRequest,
+		authCtx:        false,
+	},
+	{
+		method:         "POST",
+		url:            "/api/user",
+		body:           strings.NewReader(`{"loginOrEmail": "kek@email.kek"},`),
+		urlValues:      "",
+		expectedRes:    `{"error":"json parsing error: invalid character ',' after top-level value"}`,
+		expectedStatus: http.StatusInternalServerError,
+		authCtx:        false,
+	},
+	{
+		method:         "POST",
+		url:            "/api/user",
+		body:           strings.NewReader(`{"loginOrEmail": "kek@email.kek"}`),
+		urlValues:      "",
+		expectedRes:    `{"error":"already auth"}`,
+		expectedStatus: http.StatusBadRequest,
+		authCtx:        true,
+	},
+	{
+		method:         "POST",
+		url:            "/api/user",
+		body:           strings.NewReader(`{"loginOrEmail": "kekkekkek", "password": "kek"}`),
+		urlValues:      "",
+		expectedRes:    `{"error":"Wrong password or login"}`,
+		expectedStatus: http.StatusBadRequest,
+		authCtx:        false,
+	},
+	{
+		method:         "POST",
+		url:            "/api/user",
+		body:           strings.NewReader(`{"loginOrEmail": "kekkekkek", "password": "password"}`),
+		urlValues:      "",
+		expectedRes:    `"ok auth"`,
+		expectedStatus: http.StatusOK,
+		authCtx:        false,
+	},
+}
 
-		handler.ServeHTTP(rr, req)
-
-		// Check the status code is what we expect.
-		if status := rr.Code; status != val.expectedStatus {
-			t.Errorf("handler returned wrong status code: \ngot  %v \nwant %v",
-				status, val.expectedStatus)
-		}
-
-		// Check the response body is what we expect.
-		if rr.Body.String() != val.expectedRes {
-			t.Errorf("handler returned unexpected body: \ngot  %v \nwant %v",
-				rr.Body.String(), val.expectedRes)
-		}
+func TestSignIn(t *testing.T) {
+	err := testHandler(SignIn, testsSignIn)
+	if err != nil {
+		t.Errorf(err.Error())
 	}
 }
