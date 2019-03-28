70132230fc9f18401220a80b60ed474f421c766b
smet_k <ski97@mail.ru>
2019-03-29 01:35:48 +0300

    [configs] init server as a struct

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,17 +1,65 @@
 package server
 
 import (
+	"fmt"
 	_ "github.com/go-park-mail-ru/2019_1_5factorial-team/docs"
-	"github.com/go-park-mail-ru/2019_1_5factorial-team/internal/pkg/fileproc"
-
+	"github.com/go-park-mail-ru/2019_1_5factorial-team/internal/pkg/config_reader"
 	"github.com/go-park-mail-ru/2019_1_5factorial-team/internal/pkg/controllers"
+	"github.com/go-park-mail-ru/2019_1_5factorial-team/internal/pkg/fileproc"
 	"github.com/go-park-mail-ru/2019_1_5factorial-team/internal/pkg/middleware"
+	"github.com/go-park-mail-ru/2019_1_5factorial-team/internal/pkg/session"
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
+	"log"
 
 	"github.com/swaggo/http-swagger"
 	"net/http"
 )
+
+var instance *MyGorgeousServer
+
+type MyGorgeousServer struct {
+	StaticServerConfig fileproc.StaticServerConfig
+	CORSConfig         middleware.CORSConfig
+	CookieConfig       session.CookieConfig
+
+	configPath string
+}
+
+func (mgs *MyGorgeousServer) New(config string) *MyGorgeousServer {
+
+	mgs.configPath = config
+
+	err := config_reader.ReadConfigFile(config, "static_server_config.json", &mgs.StaticServerConfig)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "error while reading static_server_config config"))
+	}
+	mgs.StaticServerConfig.MaxUploadSize = mgs.StaticServerConfig.MaxUploadSizeMB * 1024 * 1024
+
+	log.Println("New Server->Static server config = ", mgs.StaticServerConfig)
+
+
+	err = config_reader.ReadConfigFile(config, "cors_config.json", &mgs.CORSConfig)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "error while reading CORS config"))
+	}
+
+	log.Println("New Server->CORS config = ", mgs.CORSConfig)
+
+
+	err = config_reader.ReadConfigFile(config, "cookie_config.json", &mgs.CookieConfig)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "error while reading Cookie config"))
+	}
+	fmt.Println("New Server->Cookie config = ", mgs.CookieConfig)
+
+	// чтобы не копипастить везде (это время жизни куки в часах, чтобы добавлять к уже созданной)
+	//CookieConf.CookieTimeHours = time.Duration(CookieConf.CookieDuration * int64(time.Hour))
+
+	instance = mgs
+
+	return mgs
+}
 
 func Run(port string) error {
 
