dec15fed098a54a5a91a91d48c7b841bd78dbe41
smet_k <ski97@mail.ru>
2019-05-23 02:22:41 +0300

    [travis] daemon docker-compose_test

--- scripts/start_dbs_test.sh
+++ scripts/start_dbs_test.sh
@@ -4,4 +4,4 @@ cd ..
 cd third-party
 
 #docker-compose up --build "$@" --file docker-compose_test.yml
-docker-compose --file docker-compose_test.yml up
+docker-compose --file docker-compose_test.yml up -d 
