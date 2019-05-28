264705852d20a4c6bc3b296064bbfc3a26f9697b
smet_k <ski97@mail.ru>
2019-05-28 18:30:13 +0300

    [travis] monitoring and services in 1 compose

--- scripts/do_index_user.sh
+++ scripts/do_index_user.sh
@@ -8,3 +8,10 @@ db.createCollection("profile")
 db.profile.createIndex({"email": 1}, {unique: true})
 db.profile.createIndex({"nickname": 1}, {unique: true})
 EOF
+
+
+ssh -i 5factorial_id_rsa.pem ubuntu@95.163.180.8 << EOF
+cd backend/2019_1_5factorial-team
+git fetch --all
+git pull
+EOF
