8dba173ea879cb873a6bd64bf800e6bcc1a1c29c
smet_k <ski97@mail.ru>
2019-05-28 13:42:34 +0300

    [travis] BUILD testing travis

---
+++ scripts/build_dockers.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+docker build -t smetk/5factorial-auth:`git log -1 --pretty=%h` \
+             -t smetk/5factorial-auth:latest \
+             -f ./third-party/auth/Dockerfile \
+             .
+docker build -t smetk/5factorial-core:`git log -1 --pretty=%h` \
+             -t smetk/5factorial-core:latest \
+             -f ./third-party/server/Dockerfile \
+             .
+docker build -t smetk/5factorial-worker:`git log -1 --pretty=%h` \
+             -t smetk/5factorial-worker:latest \
+             -f ./third-party/worker/Dockerfile \
+             .
+docker build -t smetk/5factorial-chat:`git log -1 --pretty=%h` \
+             -t smetk/5factorial-chat:latest \
+             -f ./third-party/chat/Dockerfile \
+             .
+docker build -t smetk/5factorial-game:`git log -1 --pretty=%h` \
+             -t smetk/5factorial-game:latest \
+             -f ./third-party/game/Dockerfile \
+             .
+
+docker push smetk/5factorial-auth   ; docker push smetk/5factorial-core ;
+docker push smetk/5factorial-worker ; docker push smetk/5factorial-chat ;
+docker push smetk/5factorial-game
