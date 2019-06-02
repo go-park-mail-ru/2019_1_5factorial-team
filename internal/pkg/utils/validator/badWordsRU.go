d7dea4701a77907bc11aec5de2ed48aa8104f483
smet_k <ski97@mail.ru>
2019-06-02 10:08:14 +0300

    [ndx-money] BUILD fix validator

--- internal/pkg/utils/validator/badWordsRU.go
+++ internal/pkg/utils/validator/badWordsRU.go
@@ -102,15 +102,18 @@ func init() {
 		regExp = fmt.Sprintf(`%s)|`, regExp)
 		//fmt.Println(regExp)
 	}
-	fmt.Println(regExp)
+	//fmt.Println(regExp)
 
-	//expBadCompile = regexp.MustCompile(regExp)
+	expBadCompile = regexp.MustCompile(regExp)
 }
 
 func ReplaceBadWords(input string) string {
 
 	//return string(expBadCompile.ReplaceAll([]byte(input), []byte("***")))
 
+	if expBadCompile == nil {
+		panic("nil compiled regexp")
+	}
 	return string(expBadCompile.ReplaceAllFunc([]byte(input), func(bytes []byte) []byte {
 		return []byte(strings.Repeat("*", len(string(bytes))/2))
 	}))
